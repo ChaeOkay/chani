08e9e61177ad6c687aafe90379f4a08df5670a35
ideahed <apprentice@devbootcamp.com>
2013-08-07 10:45:44 -0700

    add comments to controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,13 +1,16 @@
+# displays list of all short urls
+# provides form for creating a new entry (via long url input info)
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
+# takes get information, and creates new record in database
 post '/urls' do
 
   erb :confirmation
 end
 
+# display long url page from single specific database entry
 get '/:short_url' do
   params[:short_url]
 end
